0fc154ba671516200ef9a32f120a30b9185b3ba1
Joohyung Park <joohyung.park@on-the-ground.org>
2024-08-20 13:48:29 +0900

    enhanced: minor test fix

--- src/test/kotlin/org/ontheground/dmmf/ordertaking/EmailAddressSpec.kt
+++ src/test/kotlin/org/ontheground/dmmf/ordertaking/EmailAddressSpec.kt
@@ -4,10 +4,15 @@ import io.kotest.assertions.arrow.core.shouldBeLeft
 import io.kotest.assertions.arrow.core.shouldBeRight
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.booleans.shouldBeTrue
-import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 import org.ontheground.dmmf.ordertaking.ConstrainedType.isEmptyStringError
 import org.ontheground.dmmf.ordertaking.ConstrainedType.isStringPatternUnmatchedError
+
+private fun Throwable.shouldBeStringPatternUnmatchedError(): kotlin.Unit =
+    this.isStringPatternUnmatchedError().shouldBeTrue()
+
+private fun Throwable.shouldBeEmptyStringError(): kotlin.Unit =
+    this.isEmptyStringError().shouldBeTrue()
 
 
 class EmailAddressSpec : DescribeSpec({
@@ -15,21 +20,25 @@ class EmailAddressSpec : DescribeSpec({
     describe("EmailAddress") {
         context("문자열에 @ 가 포함된 경우,") {
             it("EmailAddress 객체가 정상적으로 생성된다.") {
-                EmailAddress.create("foo@bar").shouldBeRight().shouldBeInstanceOf<EmailAddress>()
+                EmailAddress.create("foo@bar")
+                    .shouldBeRight()
+                    .shouldBeInstanceOf<EmailAddress>()
             }
         }
 
         context("문자열에 @ 가 없는 경우,") {
             it("EmailAddress 객체 생성에 실패한다.") {
-                EmailAddress.create("foobar").shouldBeLeft()
-                    .isStringPatternUnmatchedError().shouldBeTrue()
+                EmailAddress.create("foobar")
+                    .shouldBeLeft()
+                    .shouldBeStringPatternUnmatchedError()
             }
         }
 
         context("문자열이 비어있는 경우,") {
             it("Value 객체 생성에 실패한다.") {
-                EmailAddress.create("").shouldBeLeft()
-                    .isEmptyStringError().shouldBeTrue()
+                EmailAddress.create("")
+                    .shouldBeLeft()
+                    .shouldBeEmptyStringError()
             }
         }
     }
