9f3aea4bd80205b9b7c93ed38f96ed3cd0b9ae98
Joohyung Park <joohyung.park@on-the-ground.org>
2024-10-28 12:01:33 +0900

    modify smart constructors using either computation block and ensure

--- src/main/kotlin/org/ontheground/dmmf/ordertaking/common/SimpleTypes.kt
+++ src/main/kotlin/org/ontheground/dmmf/ordertaking/common/SimpleTypes.kt
@@ -14,21 +14,17 @@ import org.ontheground.dmmf.ordertaking.common.ConstrainedType.isEmptyStringErro
 /// Constrained to be 50 chars or less, not null
 @JvmInline
 value class String50 private constructor(val value: String) {
-    init {
-        ConstrainedType.requireStringMaxLen(50, value)
-    }
-
     companion object {
         /// Create a String50 from a string
         /// Return Error if input is null, empty, or length > 50
-        fun create(str: String): Either<Throwable, String50> =
-            Either.catch { String50(str) }
+        operator fun invoke(str: String): Either<Throwable, String50> =
+            ConstrainedType.ensureStringMaxLen(50, str) { String50(str) }
 
         /// Create a nullable constrained string using the constructor provided
         /// Return null if input is null, empty.
         /// Return error if length > maxLen
         fun createNullable(str: String): Either<Throwable, String50?> =
-            Either.catch { String50(str) }
+            ConstrainedType.ensureStringMaxLen(50, str) { String50(str) }
                 .fold(
                     ifLeft = {
                         if (it.isEmptyStringError()) null.right()
@@ -42,76 +38,56 @@ value class String50 private constructor(val value: String) {
 /// An email address
 @JvmInline
 value class EmailAddress private constructor(val value: String) {
-    init {
-        ConstrainedType.requireStringLike(".+@.+", value)
-    }
-
     companion object {
         /// Create an EmailAddress from a string
         /// Return Error if input is null, empty, or doesn't have an "@" in it
-        fun create(str: String): Either<Throwable, EmailAddress> =
-            Either.catch { EmailAddress(str) }
+        operator fun invoke(str: String): Either<Throwable, EmailAddress> =
+            ConstrainedType.ensureStringLike(".+@.+", str) { EmailAddress(str) }
     }
 }
 
 /// A zip code
 @JvmInline
 value class ZipCode private constructor(val value: String) {
-    init {
-        ConstrainedType.requireStringLike("""\d{5}""", value)
-    }
-
     companion object {
         /// Create a ZipCode from a string
         /// Return Error if input is null, empty, or doesn't have 5 digits
-        fun create(str: String): Either<Throwable, ZipCode> =
-            Either.catch { ZipCode(str) }
+        operator fun invoke(str: String): Either<Throwable, ZipCode> =
+            ConstrainedType.ensureStringLike("""\d{5}""", str) { ZipCode(str) }
     }
 }
 
 /// An Id for Orders. Constrained to be a non-empty string <= 50 chars
 @JvmInline
 value class OrderId private constructor(val value: String) {
-    init {
-        ConstrainedType.requireStringMaxLen(50, value)
-    }
-
     companion object {
         /// Create an OrderId from a string
         /// Return Error if input is null, empty, or length > 50
-        fun create(str: String): Either<Throwable, OrderId> =
-            Either.catch { OrderId(str) }
+        operator fun invoke(str: String): Either<Throwable, OrderId> =
+            ConstrainedType.ensureStringMaxLen(50, str) { OrderId(str) }
     }
 }
 
 /// An Id for OrderLines. Constrained to be a non-empty string <= 50 chars
 @JvmInline
 value class OrderLineId private constructor(val value: String) {
-    init {
-        ConstrainedType.requireStringMaxLen(50, value)
-    }
-
     companion object {
         /// Create an OrderLineId from a string
         /// Return Error if input is null, empty, or length > 50
-        fun create(str: String): Either<Throwable, OrderLineId> =
-            Either.catch { OrderLineId(str) }
+        operator fun invoke(str: String): Either<Throwable, OrderLineId> =
+            ConstrainedType.ensureStringMaxLen(50, str) { OrderLineId(str) }
     }
 }
 
 /// The codes for Widgets start with a "W" and then four digits
 @JvmInline
 value class WidgetCode private constructor(val value: String) {
-    init {
-        ConstrainedType.requireStringLike("""W\d{4}""", value)
-    }
-
     companion object {
         /// Create an WidgetCode from a string
         /// Return Error if input is null. empty, or not matching pattern
-        fun create(str: String): Either<Throwable, WidgetCode> =
+        operator fun invoke(str: String): Either<Throwable, WidgetCode> =
             // The codes for Widgets start with a "W" and then four digits
-            Either.catch { WidgetCode(str) }
+            ConstrainedType.ensureStringLike("""W\d{4}""", str) { WidgetCode(str) }
     }
 
 }
@@ -119,16 +95,12 @@ value class WidgetCode private constructor(val value: String) {
 /// The codes for Gizmos start with a "G" and then three digits.
 @JvmInline
 value class GizmoCode private constructor(val value: String) {
-    init {
-        ConstrainedType.requireStringLike("""G\d{3}""", value)
-    }
-
     companion object {
         /// Create an GizmoCode from a string
         /// Return Error if input is null, empty, or not matching pattern
-        fun create(str: String): Either<Throwable, GizmoCode> =
+        operator fun invoke(str: String): Either<Throwable, GizmoCode> =
             // The codes for Gizmos start with a "G" and then three digits.
-            Either.catch { GizmoCode(str) }
+            ConstrainedType.ensureStringLike("""G\d{3}""", str) { GizmoCode(str) }
     }
 }
 
@@ -153,37 +125,29 @@ value class Gizmo(val value: GizmoCode) : ProductCode
 /// Return Error if input is null, empty, or not matching pattern
 fun createProductCode(code: String): Either<Throwable, ProductCode> =
     if (code.isEmpty()) Throwable("Must not be null or empty").left()
-    else if (code.startsWith("W")) WidgetCode.create(code).map { Widget(it) }
-    else if (code.startsWith("G")) GizmoCode.create(code).map { Gizmo(it) }
+    else if (code.startsWith("W")) WidgetCode(code).map { Widget(it) }
+    else if (code.startsWith("G")) GizmoCode(code).map { Gizmo(it) }
     else Throwable("Format not recognized '${code}'").left()
 
 /// Constrained to be an integer between 1 and 1000
 @JvmInline
 value class UnitQuantity private constructor(val value: Int) {
-    init {
-        ConstrainedType.requireIntInBetween(1, 1000, value)
-    }
-
     companion object {
         /// Create a UnitQuantity from an int
         /// Return Error if input is not an integer between 1 and 1000
-        fun create(i: Int): Either<Throwable, UnitQuantity> =
-            Either.catch { UnitQuantity(i) }
+        operator fun invoke(i: Int): Either<Throwable, UnitQuantity> =
+            ConstrainedType.ensureIntInBetween(1, 1000, i) { UnitQuantity(i) }
     }
 }
 
 /// Constrained to be a decimal between 0.05 and 100.00
 @JvmInline
 value class KilogramQuantity private constructor(val value: Double) {
-    init {
-        ConstrainedType.requireDoubleInBetween(0.05, 100.00, value)
-    }
-
     companion object {
         /// Create a KilogramQuantity from a decimal.
         /// Return Error if input is not a decimal between 0.05 and 100.00
-        fun create(i: Double): Either<Throwable, KilogramQuantity> =
-            Either.catch { KilogramQuantity(i) }
+        operator fun invoke(i: Double): Either<Throwable, KilogramQuantity> =
+            ConstrainedType.ensureDoubleInBetween(0.05, 100.00, i) { KilogramQuantity(i) }
     }
 }
 
@@ -208,8 +172,8 @@ fun createOrderQuantity(
     productCode: ProductCode,
 ): (Double) -> Either<Throwable, OrderQuantity> = {
     when (productCode) {
-        is Widget -> UnitQuantity.create(it.toInt()).map { i -> Unit(i) } // lift to OrderQuantity type
-        is Gizmo -> KilogramQuantity.create(it).map { i -> Kilogram(i) } // lift to OrderQuantity type
+        is Widget -> UnitQuantity(it.toInt()).map { i -> Unit(i) } // lift to OrderQuantity type
+        is Gizmo -> KilogramQuantity(it).map { i -> Kilogram(i) } // lift to OrderQuantity type
     }
 }
 
@@ -226,11 +190,11 @@ value class Price private constructor(val value: Double) {
     /// Multiply a Price by a decimal qty.
     /// Return Error if new price is out of bounds.
     fun multiply(qty: Double): Either<Throwable, Price> =
-        create(qty * this.value)
+        Price.invoke(qty * this.value)
 
 
     companion object {
-        fun create(p: Double): Either<Throwable, Price> =
+        operator fun invoke(p: Double): Either<Throwable, Price> =
         /// Create a Price from a decimal.
             /// Throw an exception if out of bounds. This should only be used if you know the value is valid.
             Either.catch { Price(p) }
@@ -251,20 +215,16 @@ value class Price private constructor(val value: Double) {
 /// Constrained to be a decimal between 0.0 and 10000.00
 @JvmInline
 value class BillingAmount private constructor(val value: Double) {
-    init {
-        ConstrainedType.requireDoubleInBetween(0.0, 10000.00, value)
-    }
-
     companion object {
         /// Create a BillingAmount from a decimal.
         /// Return Error if input is not a decimal between 0.0 and 10000.00
-        fun create(i: Double): Either<Throwable, BillingAmount> =
-            Either.catch { BillingAmount(i) }
+        operator fun invoke(i: Double): Either<Throwable, BillingAmount> =
+            ConstrainedType.ensureDoubleInBetween(0.0, 10000.00, i) { BillingAmount(i) }
 
 
         /// Sum a list of prices to make a billing amount
         /// Return Error if total is out of bounds
         fun sumPrices(prices: Array<Price>): Either<Throwable, BillingAmount> =
-            create(prices.sumOf { it.value })
+            invoke(prices.sumOf { it.value })
     }
 }
--- src/test/kotlin/org/ontheground/dmmf/ordertaking/EmailAddressSpec.kt
+++ src/test/kotlin/org/ontheground/dmmf/ordertaking/EmailAddressSpec.kt
@@ -21,7 +21,7 @@ class EmailAddressSpec : DescribeSpec({
     describe("EmailAddress") {
         context("문자열에 @ 가 포함된 경우,") {
             it("EmailAddress 객체가 정상적으로 생성된다.") {
-                EmailAddress.create("foo@bar")
+                EmailAddress("foo@bar")
                     .shouldBeRight()
                     .shouldBeInstanceOf<EmailAddress>()
             }
@@ -29,7 +29,7 @@ class EmailAddressSpec : DescribeSpec({
 
         context("문자열에 @ 가 없는 경우,") {
             it("EmailAddress 객체 생성에 실패한다.") {
-                EmailAddress.create("foobar")
+                EmailAddress("foobar")
                     .shouldBeLeft()
                     .shouldBeStringPatternUnmatchedError()
             }
@@ -37,7 +37,7 @@ class EmailAddressSpec : DescribeSpec({
 
         context("문자열이 비어있는 경우,") {
             it("Value 객체 생성에 실패한다.") {
-                EmailAddress.create("")
+                EmailAddress("")
                     .shouldBeLeft()
                     .shouldBeEmptyStringError()
             }
