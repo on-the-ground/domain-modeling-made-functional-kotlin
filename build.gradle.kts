3a4966a63f83071edbfdcd859723aa6e5b033187
Joohyung Park <joohyung.park@on-the-ground.org>
2025-08-08 12:32:25 +0900

    feat: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,63 +1,75 @@
+// build.gradle.kts
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
-	kotlin("jvm") version "2.2.0"
-	kotlin("kapt") version "2.2.0"
-	kotlin("plugin.serialization") version "2.2.0"
+    kotlin("jvm") version "2.2.0"
+    kotlin("plugin.serialization") version "2.2.0"
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2"
+
+    id("com.github.ben-manes.versions") version "0.52.0"
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
 }
 
 group = "org.ontheground"
 version = "0.0.1"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
-
 val kotestVersion = "5.9.1"
-val arrowVersion = "1.2.4"
-val exposedVersion = "0.56.0"
-val postgresDriverVersion = "42.7.4"
+val arrowVersion = "2.1.2"
+val exposedVersion = "0.61.0"
+val postgresDriverVersion = "42.7.7"
+
 dependencies {
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
-	implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
-	implementation("io.arrow-kt:arrow-core:$arrowVersion")
-	implementation("io.arrow-kt:arrow-optics:$arrowVersion")
-	implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-	implementation("org.jetbrains.exposed:exposed-crypt:$exposedVersion")
-	implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
-	implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-	implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
-	implementation("org.jetbrains.exposed:exposed-json:$exposedVersion")
-	implementation("org.postgresql:postgresql:$postgresDriverVersion")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.7.1")
 
-	kapt("io.arrow-kt:arrow-meta:1.6.2")
+    // Arrow
+    implementation("io.arrow-kt:arrow-core:$arrowVersion")
+    implementation("io.arrow-kt:arrow-optics:$arrowVersion")
+    ksp("io.arrow-kt:arrow-optics-ksp-plugin:$arrowVersion")
 
-	testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
-	testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
-	testImplementation("io.kotest:kotest-framework-api:$kotestVersion")
-	testImplementation("io.kotest.extensions:kotest-assertions-arrow:1.4.0")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // Exposed
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-crypt:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-json:$exposedVersion")
+    implementation("org.postgresql:postgresql:$postgresDriverVersion")
+
+    // 테스트
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-framework-api:$kotestVersion")
+    testImplementation("io.kotest.extensions:kotest-assertions-arrow:2.0.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict", "-Xcontext-parameters")
-		languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_2)
-		apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_2)
-	}
-}
-
-kapt {
-	useBuildCache = false
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict", "-Xcontext-parameters")
+        languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_2)
+        apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_2)
+    }
 }
 
 tasks.test {
-	useJUnitPlatform()
+    useJUnitPlatform()
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        val v = candidate.version.lowercase()
+        listOf("alpha", "beta", "rc", "m", "milestone").any { tag -> v.contains(tag) }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
 rootProject.name = "dmmf"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal() // ← KSP 플러그인 여기서 받아옴
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
